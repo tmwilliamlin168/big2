5a5040d2f79e20d74b30ef76996cf82435c5bb32
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-02-13 13:31:03 +0800

    Update game.tsx
    proper socket io client use

--- front/pages/game.tsx
+++ front/pages/game.tsx
@@ -1,9 +1,9 @@
 import {useEffect} from 'react'
-import {io} from 'socket.io-client'
+import io from 'socket.io-client'
 
 export default function Game() {
 	useEffect(() => {
-		const socket = io(process.env.NEXT_PUBLIC_BACK_HOST);
-		return () => socket.close();
+		const socket = io(process.env.NEXT_PUBLIC_BACK_HOST!);
+		return () => {socket.close()};
 	}, []);
 }
