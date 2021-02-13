721cb4a7a3ddd4455ce5f4bac59a3a077bbd8461
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-02-13 23:49:44 +0800

    basic username login

--- back/src/index.ts
+++ back/src/index.ts
@@ -5,9 +5,13 @@ dotenv.config();
 
 const server = new Server({cors: {origin: process.env.ORIGIN, methods: ['GET', 'POST']}});
 
-server.on('connection', (client: Socket) => {
-	console.log('client connect');
-	client.on('disconnect', () => console.log('disconnect'))
+server.on('connection', (socket: Socket) => {
+	console.log('client connect', socket.id);
+	socket.on('login', username => {
+		console.log('login', username);
+		socket.emit('loginRes', {success: true});
+	});
+	socket.on('disconnect', () => console.log('disconnect'))
 });
 
 server.listen(+process.env.PORT! || 6900);
--- front/pages/game.tsx
+++ front/pages/game.tsx
@@ -1,11 +1,78 @@
-import {useEffect} from 'react'
-import io from 'socket.io-client'
+import {useEffect, useState} from 'react';
+import io, {Socket} from 'socket.io-client';
 
-export default function Game() {
+const LoginForm = ({finish, socket, username}: {finish: (s: string) => void, socket: Socket, username: string | null}) => {
+	const [value, setValue] = useState(username || '');
+	const [waiting, setWaiting] = useState(false);
+	const [error, setError] = useState('');
+
+	const login = () => {
+		setWaiting(true);
+		socket.emit('login', value);
+		socket.once('loginRes', data => {
+			setWaiting(false);
+			if(data.success)
+				finish(value);
+			else
+				setError(data.error);
+		});
+	};
+
 	useEffect(() => {
-		const socket = io(process.env.NEXT_PUBLIC_BACK_HOST!);
-		return () => {socket.close()};
+		if (username) login();
 	}, []);
 
-	return null;
+	return (
+		<>
+			<input
+				type="text"
+				maxLength={15}
+				placeholder="Your username..."
+				value={value}
+				onChange={(e) => setValue(e.target.value)}
+				disabled={waiting}
+			/>
+			<button onClick={login} disabled={!value || waiting}>
+				Ok
+			</button>
+			{waiting && 'Logging in...'}
+			<p style={{color: 'red'}}>{error}</p>
+		</>
+	);
+};
+
+export default function Game() {
+	const [socket, setSocket] = useState(null);
+	const [connected, setConnected] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false);
+	const [username, setUsername] = useState(null);
+
+	useEffect(() => {
+		if(!socket) {
+			console.log('create');
+			setSocket(io(process.env.NEXT_PUBLIC_BACK_HOST!));
+			return;
+		}
+		socket.on('connect', () => setConnected(true));
+		socket.on('disconnect', () => {
+			setConnected(false);
+			setLoggedIn(false);
+		});
+		return () => {socket.close(); alert('plz reload')};
+	}, [socket]);
+
+	if (!socket) return null;
+	if (!loggedIn) {
+		return (
+			<LoginForm
+				socket={socket}
+				finish={(s) => {
+					setLoggedIn(true);
+					setUsername(s);
+				}}
+				username={username}
+			/>
+		);
+	}
+	return connected?'Connected':'Connecting';
 }
