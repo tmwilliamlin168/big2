deffe368f42a696d5f6c59d298d71e7810282286
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-02-13 14:18:21 +0800

    fix cors

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,9 +1,12 @@
+import dotenv from 'dotenv';
 import {Server, Socket} from 'socket.io';
 
-const server = new Server();
+dotenv.config();
+
+const server = new Server({cors: {origin: process.env.ORIGIN, methods: ['GET', 'POST']}});
 
 server.on('connection', (client: Socket) => {
-	console.log(client);
+	console.log('client connect');
 	client.on('disconnect', () => console.log('disconnect'))
 });
 
