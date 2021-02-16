d7de23f5bd8acac1aeb546556b8ec3b4dc3598d7
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2021-02-17 01:07:15 +0800

    Update game.tsx
    fix when cardSelected is updated later and cardSelected[i] is undefined

--- front/pages/game.tsx
+++ front/pages/game.tsx
@@ -104,7 +104,7 @@ export default function Game() {
 					<label key={card.rank+' '+card.suit}>
 						<input
 							type="checkbox"
-							checked={cardSelected[i]}
+							checked={cardSelected[i] || false}
 							onChange={() => {
 								const cardSelected2 = [...cardSelected];
 								cardSelected2[i] = !cardSelected[i];
